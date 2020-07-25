6ebbce6860daf720a9369aaaf08efd408db500df
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-25 16:15:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_data = {
+   :Theo => {
+     ["purple", "grey", "male", "subway"]
+  }
+    :Peter Jr. => {
+      ["purple", "grey", "male", "Library"]
+    } 
+    :Lucky => {
+      ["purple", "male","Central Park"]
+    } 
+    :Queenie => {
+      ["white", "female", "Subway"]
+    }
+    :Ms. K => {
+      ["grey", "white", "Central Park"]
+    }
+  }# write your code here!
 end
